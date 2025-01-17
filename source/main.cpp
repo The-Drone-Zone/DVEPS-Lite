4b47ab53bb8040add2d1f6e7bed434705db2f859
dshay <dorianshay0806@arizona.edu>
2025-01-16 20:12:18 -0700

    updated workflow, added initial test file and build file

---
+++ source/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdlib>
+
+int main(int argc, char* argv[]) {
+    std::cout << "Hello, World!" << std::endl;
+
+    if (argc != 3) {
+        std::cerr << "Usage: " << argv[0] << " <num1> <num2>" << std::endl;
+        return 1;
+    }
+
+    int num1 = std::atoi(argv[1]);
+    int num2 = std::atoi(argv[2]);
+
+    std::cout << "The sum of " << num1 << " and " << num2 << " is " << (num1 + num2) << std::endl;
+
+    return 0;
+}
