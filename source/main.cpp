5d5432958d813978a4bc218130acdae4de8ff97e
dshay <dorianshay0806@arizona.edu>
2025-01-18 17:01:08 -0700

    expecting to see fail for formatting

--- source/main.cpp
+++ source/main.cpp
@@ -1,12 +1,13 @@
 #include <cstdlib>
 #include <iostream>
 
-int main(int argc, const char* argv[]) {
+int     main( int argc, const char* argv[]) {
     std::cout << "Hello, World!" << std::endl;
 
     if (argc != 3) {
+        
         std::cerr << "Usage: " << argv[0] << " <num1> <num2>" << std::endl;
-        return 1;
+            return 1;
     }
 
     int num1 = std::atoi(argv[1]);
