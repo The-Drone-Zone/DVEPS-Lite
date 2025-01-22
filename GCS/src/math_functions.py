69efa02f398cab10437a91e4f8b764c8a9dacae8
aroig <aroig@arizona.edu>
2025-01-22 14:00:07 -0700

    python ci

---
+++ GCS/src/math_functions.py
@@ -0,0 +1,3 @@
+class Math:
+    def add(num1, num2):
+        return num1 + num2
---
+++ GCS/tests/math_test.py
@@ -0,0 +1,8 @@
+import pytest
+from src.math_functions import Math
+
+def test_add():
+    test = Math()
+    assert test.add(2, 3) == 5
+    assert test.add(-1, 1) == 0
+    assert test.add(0, 0) == 0
