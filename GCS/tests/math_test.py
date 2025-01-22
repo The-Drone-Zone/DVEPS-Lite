f27b6c79334e348c1c835a94a1a75408c6beaa62
aroig <aroig@arizona.edu>
2025-01-22 15:08:41 -0700

    final ci

--- GCS/tests/math_test.py
+++ GCS/tests/math_test.py
@@ -2,8 +2,16 @@ import pytest
 from src import Math
 
 
-def test_add():
+def test_add_positive():
     test = Math()
     assert test.add(2, 3) == 5
+
+
+def test_add_negative():
+    test = Math()
     assert test.add(-1, 1) == 0
+
+
+def test_add_zero():
+    test = Math()
     assert test.add(0, 0) == 0
