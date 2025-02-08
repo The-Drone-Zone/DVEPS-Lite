e6856fee51fc802aac6ce2c4ad0e2988eb03b375
dshay <dorianshay0806@arizona.edu>
2025-02-08 16:57:41 -0700

    formatted code

--- processor/src/main.cpp
+++ processor/src/main.cpp
@@ -1,7 +1,7 @@
 #include <cstdlib>
 #include <iostream>
-#include <opencv4/opencv2/opencv.hpp>
 #include <opencv4/opencv2/core/cuda.hpp>
+#include <opencv4/opencv2/opencv.hpp>
 
 int main(int argc, const char* argv[]) {
     std::cout << "Hello, World!" << std::endl;
@@ -15,7 +15,6 @@ int main(int argc, const char* argv[]) {
     int num2 = std::atoi(argv[2]);
 
     std::cout << "The sum of " << num1 << " and " << num2 << " is " << (num1 + num2) << std::endl;
-
 
     std::cout << "OpenCV version: " << CV_VERSION << std::endl;
 
