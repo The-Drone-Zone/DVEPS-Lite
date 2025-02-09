e5b401567ed2e46469016f9cd0b4f4cc476b2366
dshay22 <124942574+dshay22@users.noreply.github.com>
2025-02-08 19:32:13 -0700

    Making dev environment (#26)
    * containr made
    
    * deleted .vscode folder
    
    * changed the order of git ignore
    
    * formatted code
    
    * updated CI: test 1
    
    * forgot to cd into the right directory: test 2
    
    ---------
    
    Co-authored-by: dshay <dorianshay0806@arizona.edu>

--- processor/src/main.cpp
+++ processor/src/main.cpp
@@ -1,5 +1,7 @@
 #include <cstdlib>
 #include <iostream>
+#include <opencv4/opencv2/core/cuda.hpp>
+#include <opencv4/opencv2/opencv.hpp>
 
 int main(int argc, const char* argv[]) {
     std::cout << "Hello, World!" << std::endl;
@@ -13,6 +15,12 @@ int main(int argc, const char* argv[]) {
     int num2 = std::atoi(argv[2]);
 
     std::cout << "The sum of " << num1 << " and " << num2 << " is " << (num1 + num2) << std::endl;
+
+    std::cout << "OpenCV version: " << CV_VERSION << std::endl;
+
+    // Check if CUDA is available
+    int cudaDeviceCount = cv::cuda::getCudaEnabledDeviceCount();
+    std::cout << "CUDA-enabled devices found: " << cudaDeviceCount << std::endl;
 
     return 0;
 }
