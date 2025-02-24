9e556593d4789e33e462115411c3d99077522b8f
aroig <aroig@arizona.edu>
2025-02-23 18:10:14 -0700

    decision package stub

--- processor/src/decision_pkg/src/decision_node.cpp
+++ processor/src/decision_pkg/src/decision_node.cpp
@@ -6,6 +6,7 @@
 #include "std_msgs/msg/header.hpp"
 #include "camera_scan_pkg/msg/obstacle.hpp"
 #include "camera_scan_pkg/msg/obstacle_array.hpp"
+#include "custom_msg_pkg/msg/lidar_position.hpp"
 
 class DecisionController : public rclcpp::Node {
     public:
@@ -13,18 +14,27 @@ class DecisionController : public rclcpp::Node {
         long int counter = 0;
 
         DecisionController() : Node("decision_node") {
-            // Create Subscriber
-            subscription_ = this->create_subscription<camera_scan_pkg::msg::ObstacleArray>(
-                "output_obstacles", 10, std::bind(&DecisionController::decisionCallback, this, std::placeholders::_1));
+            // Create Subscriber to Image Analysis
+            image_subscription_ = this->create_subscription<camera_scan_pkg::msg::ObstacleArray>(
+                "output_obstacles", 10, std::bind(&DecisionController::imageCallback, this, std::placeholders::_1));
+            
+            // Create Subscriber to LiDAR Analysis
+            lidar_subscription_ = this->create_subscription<custom_msg_pkg::msg::LidarPosition>(
+                "output_obstacles", 10, std::bind(&DecisionController::lidarCallback, this, std::placeholders::_1));
         }
     private:
-        decisionCallback(const camera_scan_pkg::msg::ObstacleArray::SharedPtr msg) {
+        rclcpp::Subscription<camera_scan_pkg::msg::ObstacleArray>::SharedPtr image_subscription_;
+        rclcpp::Subscription<custom_msg_pkg::msg::LidarPosition>::SharedPtr lidar_subscription_;
+
+        void imageCallback(const camera_scan_pkg::msg::ObstacleArray::SharedPtr msg) {
             // Start time
             auto start = std::chrono::high_resolution_clock::now();
 
-            RCLCPP_INFO(this->get_logger(), "Received %zu analyzed image obstacles", msg.obstacles.size());
+            RCLCPP_INFO(this->get_logger(), "Received %zu analyzed image obstacles", msg->obstacles.size());
 
-            printObstacles(msg);
+            printImageObstacles(msg);
+
+            // Image to LiDAR map function call goes here
 
             // End time
             auto end = std::chrono::high_resolution_clock::now();
@@ -37,8 +47,27 @@ class DecisionController : public rclcpp::Node {
             RCLCPP_INFO(this->get_logger(), "Current Average Decision FPS: %ld fps", 1000 / (time_sum / counter));
         }
 
-    void printObstacles(const camera_scan_pkg::msg::ObstacleArray::SharedPtr msg) {
-        for (camera_scan_pkg::msg::Obstacle obstacle : msg.obstacles) {
+        void lidarCallback(const custom_msg_pkg::msg::LidarPosition::SharedPtr msg) {
+            // Start time
+            auto start = std::chrono::high_resolution_clock::now();
+
+            RCLCPP_INFO(this->get_logger(), "Received %zu analyzed LiDAR samples", msg->x.size());
+
+            // Image to LiDAR map function call goes here
+
+            // End time
+            auto end = std::chrono::high_resolution_clock::now();
+            // Compute duration
+            std::chrono::milliseconds duration_ms = std::chrono::duration_cast<std::chrono::milliseconds >(end - start);
+            // Print analysis time
+            RCLCPP_INFO(this->get_logger(), "Decision Time: %ld ms", duration_ms.count());
+            time_sum += duration_ms.count();
+            counter += 1;
+            RCLCPP_INFO(this->get_logger(), "Current Average Decision FPS: %ld fps", 1000 / (time_sum / counter));
+        }
+
+    void printImageObstacles(const camera_scan_pkg::msg::ObstacleArray::SharedPtr msg) {
+        for (camera_scan_pkg::msg::Obstacle obstacle : msg->obstacles) {
             RCLCPP_INFO(this->get_logger(), "Obstacle detect at: (%lf, %lf)", obstacle.x, obstacle.y);
         }
     }
@@ -46,7 +75,7 @@ class DecisionController : public rclcpp::Node {
 
 int main(int argc, char** argv) {
     rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<ImageAnalysis>());
+    rclcpp::spin(std::make_shared<DecisionController>());
     rclcpp::shutdown();
     return 0;
 }
