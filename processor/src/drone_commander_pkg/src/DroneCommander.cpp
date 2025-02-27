00ecc67b62d97244095f08eea26354b8b04660f0
dveps-lite software team <dorianshay0806@gamil.com>
2025-02-27 11:26:57 -0700

    added PX4 msg package as submodule, added new package for controlling in offboard mode

---
+++ processor/src/drone_commander_pkg/src/DroneCommander.cpp
@@ -0,0 +1,28 @@
+#include <px4_msgs/msg/offboard_control_mode.hpp>
+#include <px4_msgs/msg/trajectory_setpoint.hpp>
+#include <px4_msgs/msg/vehicle_command.hpp>
+#include <px4_msgs/msg/vehicle_control_mode.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include <stdint.h>
+
+
+#include <chrono>
+#include <iostream>
+
+
+using namespace std::chrono;
+using namespace std::chrono_literals;
+using namespace px4_msgs::msg;
+
+
+class DroneCommander : public rclcpp::Node
+{
+    DroneCommander(): Node("drone_commander")
+    {
+        offboard_control_mode_publisher_ = this->create_publisher<OffboardControlMode>("/fmu/in/offboard_control_mode", 10);
+		trajectory_setpoint_publisher_ = this->create_publisher<TrajectorySetpoint>("/fmu/in/trajectory_setpoint", 10);
+		vehicle_command_publisher_ = this->create_publisher<VehicleCommand>("/fmu/in/vehicle_command", 10);
+
+        
+    }
+}
