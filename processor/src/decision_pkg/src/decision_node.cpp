d24e93de82cd2891f6495d9f2b2cf96121dcda1a
dshay <dorianshay0806@arizona.edu>
2025-02-28 09:12:27 -0700

    small message control test

--- processor/src/decision_pkg/src/decision_node.cpp
+++ processor/src/decision_pkg/src/decision_node.cpp
@@ -7,6 +7,7 @@
 #include "camera_scan_pkg/msg/obstacle.hpp"
 #include "camera_scan_pkg/msg/obstacle_array.hpp"
 #include "custom_msg_pkg/msg/lidar_position.hpp"
+#include "custom_msg_pkg/msg/command.hpp"
 
 class DecisionController : public rclcpp::Node {
     public:
@@ -21,10 +22,14 @@ class DecisionController : public rclcpp::Node {
             // Create Subscriber to LiDAR Analysis
             lidar_subscription_ = this->create_subscription<custom_msg_pkg::msg::LidarPosition>(
                 "Lidar/analysis", 10, std::bind(&DecisionController::lidarCallback, this, std::placeholders::_1));
+
+            control_publisher_ = this->create_publisher<custom_msg_pkg::msg::Command>("DecisionController/command", 10);
         }
     private:
         rclcpp::Subscription<camera_scan_pkg::msg::ObstacleArray>::SharedPtr image_subscription_;
         rclcpp::Subscription<custom_msg_pkg::msg::LidarPosition>::SharedPtr lidar_subscription_;
+        rclcpp::Publisher<custom_msg_pkg::msg::Control>::SharedPtr control_publisher_;
+        bool temp_bool = true;
 
         void imageCallback(const camera_scan_pkg::msg::ObstacleArray::SharedPtr msg) {
             // Start time
@@ -33,6 +38,14 @@ class DecisionController : public rclcpp::Node {
             RCLCPP_INFO(this->get_logger(), "Received %zu analyzed image obstacles", msg->obstacles.size());
 
             //printImageObstacles(msg);
+            if(temp_bool) {
+                if(msg->obstacles.size() > 8) {
+                    RCLCPP_INFO(this->get_logger(), "STOP STOP STOP");
+                    publish_control_command(custom_msg_pkg::msg::Command::STOP);
+                    temp_bool = false;
+                }
+            }
+            
 
             // Image to LiDAR map function call goes here
 
@@ -66,11 +79,17 @@ class DecisionController : public rclcpp::Node {
             RCLCPP_INFO(this->get_logger(), "Current Average Decision FPS: %ld fps", 1000 / (time_sum / counter));
         }
 
-    void printImageObstacles(const camera_scan_pkg::msg::ObstacleArray::SharedPtr msg) {
-        for (camera_scan_pkg::msg::Obstacle obstacle : msg->obstacles) {
-            RCLCPP_INFO(this->get_logger(), "Obstacle detect at: (%lf, %lf)", obstacle.x, obstacle.y);
+        void publish_control_command(const custom_msg_pkg::msg::Command &command) {
+            custom_msg_pkg::msg::Control control_msg;
+            control_msg.command = command;
+            control_publisher->publish(control_msg);
         }
-    }
+
+        void printImageObstacles(const camera_scan_pkg::msg::ObstacleArray::SharedPtr msg) {
+            for (camera_scan_pkg::msg::Obstacle obstacle : msg->obstacles) {
+                RCLCPP_INFO(this->get_logger(), "Obstacle detect at: (%lf, %lf)", obstacle.x, obstacle.y);
+            }
+        }
 };
 
 int main(int argc, char** argv) {
--- processor/src/drone_commander_pkg/src/DroneCommander.cpp
+++ processor/src/drone_commander_pkg/src/DroneCommander.cpp
@@ -17,12 +17,123 @@ using namespace px4_msgs::msg;
 
 class DroneCommander : public rclcpp::Node
 {
-    DroneCommander(): Node("drone_commander")
-    {
-        offboard_control_mode_publisher_ = this->create_publisher<OffboardControlMode>("/fmu/in/offboard_control_mode", 10);
-		trajectory_setpoint_publisher_ = this->create_publisher<TrajectorySetpoint>("/fmu/in/trajectory_setpoint", 10);
-		vehicle_command_publisher_ = this->create_publisher<VehicleCommand>("/fmu/in/vehicle_command", 10);
+    public:
+        DroneCommander(): Node("drone_commander")
+        {
+            offboard_control_mode_publisher_ = this->create_publisher<OffboardControlMode>("/fmu/in/offboard_control_mode", 10);
+            trajectory_setpoint_publisher_ = this->create_publisher<TrajectorySetpoint>("/fmu/in/trajectory_setpoint", 10);
+            vehicle_command_publisher_ = this->create_publisher<VehicleCommand>("/fmu/in/vehicle_command", 10);
+            decision_command_subscriber_ = this->create_subscription<custom_msg_pkg::msg::Command>("DecisionController/command", 10, std::bind(&DroneCommander::checkCommand, this, std::placeholders::_1));
+            
+            timer = this->create_wall_timer(100ms, std::bind(&DroneCommander::CommanderCallback, this));
+            offboard_setpoint_counter_ = 0;
+        }
 
-        
-    }
-}
+        void arm()
+        {
+            publish_vehicle_command(VehicleCommand::VEHICLE_CMD_COMPONENT_ARM_DISARM, 1.0);
+
+            RCLCPP_INFO(this->get_logger(), "Arm command send");
+        }
+
+        void disarm()
+        {
+            publish_vehicle_command(VehicleCommand::VEHICLE_CMD_COMPONENT_ARM_DISARM, 0.0);
+
+            RCLCPP_INFO(this->get_logger(), "Disarm command send");
+        }
+
+    private:
+        int offboard_setpoint_counter_;
+        rclcpp::TimerBase::SharedPtr timer_;
+        rclcpp::Publisher<OffboardControlMode>::SharedPtr offboard_control_mode_publisher_;
+        rclcpp::Publisher<TrajectorySetpoint>::SharedPtr trajectory_setpoint_publisher_;
+        rclcpp::Publisher<VehicleCommand>::SharedPtr vehicle_command_publisher_;
+        rclcpp::Subscription<custom_msg_pkg::msg::Command>::SharedPtr decision_command_subscriber_;
+
+        bool stop_flag_ = false;
+
+        void checkCommand(const custom_msg_pkg::msg::Command::SharedPtr msg) {
+            if(msg->command == custom_msg_pkg::msg::Command::STOP) {
+                RCLCPP_INFO(this->get_logger(), "Received Command: %d", msg->command);
+                stop_flag_ = true;
+            }
+            else {
+                RCLCPP_INFO(this->get_logger(), "NO STOP");
+            }
+        }
+
+        void CommanderCallback()
+        {
+            if (offboard_setpoint_counter_ == 10) {
+                // Change to Offboard mode after 10 setpoints
+                this->publish_vehicle_command(VehicleCommand::VEHICLE_CMD_DO_SET_MODE, 1, 6);
+
+                // Arm the vehicle
+                this->arm();
+            }
+
+            if(stop_flag_) {
+                RCLCPP_INFO(this->get_logger(), "STOPPING");
+            }
+            else
+            {
+                // offboard_control_mode needs to be paired with trajectory_setpoint
+                publish_offboard_control_mode();
+                publish_trajectory_setpoint();
+            }
+
+            // stop the counter after reaching 11
+            if (offboard_setpoint_counter_ < 11) {
+                offboard_setpoint_counter_++;
+            }
+        }
+
+        void publish_offboard_control_mode()
+        {
+            OffboardControlMode msg{};
+            msg.position = true;
+            msg.velocity = true;
+            msg.acceleration = false;
+            msg.attitude = false;
+            msg.body_rate = false;
+            msg.timestamp = this->get_clock()->now().nanoseconds() / 1000;
+            offboard_control_mode_publisher_->publish(msg);
+        }
+
+        void publish_trajectory_setpoint()
+        {
+            TrajectorySetpoint msg{};
+            msg.position = {0.0, 0.0, -5.0};
+            msg.yaw = -3.14; // [-PI:PI]
+            msg.timestamp = this->get_clock()->now().nanoseconds() / 1000;
+            trajectory_setpoint_publisher_->publish(msg);
+        }
+
+        void publish_vehicle_command(uint16_t command, float param1, float param2)
+        {
+            VehicleCommand msg{};
+            msg.param1 = param1;
+            msg.param2 = param2;
+            msg.command = command;
+            msg.target_system = 1;
+            msg.target_component = 1;
+            msg.source_system = 1;
+            msg.source_component = 1;
+            msg.from_external = true;
+            msg.timestamp = this->get_clock()->now().nanoseconds() / 1000;
+            vehicle_command_publisher_->publish(msg);
+        }
+};
+
+
+int main(int argc, char *argv[])
+{
+	std::cout << "Starting DroneCommander node..." << std::endl;
+	setvbuf(stdout, NULL, _IONBF, BUFSIZ);
+	rclcpp::init(argc, argv);
+	rclcpp::spin(std::make_shared<DroneCommander>());
+
+	rclcpp::shutdown();
+	return 0;
+}   
