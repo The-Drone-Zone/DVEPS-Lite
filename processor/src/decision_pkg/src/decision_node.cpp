1ea9596cca566acdfed26707dabcd2d638b45fb4
dshay22 <124942574+dshay22@users.noreply.github.com>
2025-03-20 20:28:20 -0700

    Offboard command node (#37)
    * added PX4 msg package as submodule, added new package for controlling in offboard mode
    
    * small message control test
    
    * Added smooth stop ability and a  way to resume after stopping
    
    * trying to turn and move forward
    
    * just saving work for now
    
    * drone stops and retunrns to landing, commanded by decision controller dummy logic example
    
    * new position control package
    
    * correctly built stuff
    
    * making small updates to try and getting turnin gto work
    
    * had to edit the way ros node handled another class
    
    * moved the pos_pkg into drone commander as ti is not a node anymore
    
    * fixed spelling stuff
    
    * ironed out some kinks
    
    * final push
    
    ---------
    
    Co-authored-by: dveps-lite software team <dorianshay0806@gamil.com>
    Co-authored-by: dshay <dorianshay0806@arizona.edu>

--- processor/src/decision_pkg/src/decision_node.cpp
+++ processor/src/decision_pkg/src/decision_node.cpp
@@ -1,12 +1,25 @@
-#include <iostream>
+#include <atomic>
 #include <chrono>
+#include <condition_variable>
+#include <iostream>
+#include <thread>
 
-#include "rclcpp/rclcpp.hpp"
-#include "sensor_msgs/msg/image.hpp"
-#include "std_msgs/msg/header.hpp"
 #include "camera_scan_pkg/msg/obstacle.hpp"
 #include "camera_scan_pkg/msg/obstacle_array.hpp"
 #include "custom_msg_pkg/msg/lidar_position.hpp"
+#include "custom_msg_pkg/msg/command.hpp"
+#include "custom_msg_pkg/msg/command_ack.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/image.hpp"
+#include "std_msgs/msg/header.hpp"
+
+enum STATES {
+    DEFAULT = 0,
+    STOP,
+    TURN,
+    REROUTING,
+    FORWARD
+};
 
 class DecisionController : public rclcpp::Node {
     public:
@@ -21,10 +34,37 @@ class DecisionController : public rclcpp::Node {
             // Create Subscriber to LiDAR Analysis
             lidar_subscription_ = this->create_subscription<custom_msg_pkg::msg::LidarPosition>(
                 "Lidar/analysis", 10, std::bind(&DecisionController::lidarCallback, this, std::placeholders::_1));
+
+            command_publisher_ = this->create_publisher<custom_msg_pkg::msg::Command>("DecisionController/command", 10);
+            command_ack_subscrption_ = this->create_subscription<custom_msg_pkg::msg::CommandAck>("DecisionController/command_ack", 10, std::bind(&DecisionController::command_ack_callback, this, std::placeholders::_1));
+
+            find_path_thread = std::thread(&DecisionController::find_path, this);
+            decision_mode.store(STATES::DEFAULT, std::memory_order_release); //DEAFULT==0
+            current_state.store(STATES::DEFAULT, std::memory_order_release); //DEAFULT==0
+
         }
+
+
     private:
+        rclcpp::TimerBase::SharedPtr timer_;
         rclcpp::Subscription<camera_scan_pkg::msg::ObstacleArray>::SharedPtr image_subscription_;
         rclcpp::Subscription<custom_msg_pkg::msg::LidarPosition>::SharedPtr lidar_subscription_;
+        rclcpp::Publisher<custom_msg_pkg::msg::Command>::SharedPtr command_publisher_;
+        rclcpp::Subscription<custom_msg_pkg::msg::CommandAck>::SharedPtr command_ack_subscrption_;
+
+        std::thread find_path_thread;
+
+
+        std::atomic<STATES> decision_mode;
+        std::atomic<STATES> current_state;
+        
+
+        std::atomic<bool> outstanding_ack;
+
+        camera_scan_pkg::msg::ObstacleArray image_obstacles;
+        custom_msg_pkg::msg::LidarPosition lidar_samples;
+        int turn_count = 0;
+
 
         void imageCallback(const camera_scan_pkg::msg::ObstacleArray::SharedPtr msg) {
             // Start time
@@ -33,6 +73,14 @@ class DecisionController : public rclcpp::Node {
             RCLCPP_INFO(this->get_logger(), "Received %zu analyzed image obstacles", msg->obstacles.size());
 
             //printImageObstacles(msg);
+            image_obstacles = *msg;
+
+            if(msg->tracked_obstacle && current_state.load(std::memory_order_acquire) == 0 && !outstanding_ack.load(std::memory_order_acquire)) {
+                RCLCPP_INFO(this->get_logger(), "STOP STOP STOP");
+                publish_control_command(custom_msg_pkg::msg::Command::STOP);
+                outstanding_ack.store(true, std::memory_order_release); 
+            }
+            
 
             // Image to LiDAR map function call goes here
 
@@ -41,10 +89,10 @@ class DecisionController : public rclcpp::Node {
             // Compute duration
             std::chrono::milliseconds duration_ms = std::chrono::duration_cast<std::chrono::milliseconds >(end - start);
             // Print analysis time
-            RCLCPP_INFO(this->get_logger(), "Decision Time: %ld ms", duration_ms.count());
+            // RCLCPP_INFO(this->get_logger(), "Decision Time: %ld ms", duration_ms.count());
             time_sum += duration_ms.count();
             counter += 1;
-            RCLCPP_INFO(this->get_logger(), "Current Average Decision FPS: %ld fps", 1000 / (time_sum / counter));
+            // RCLCPP_INFO(this->get_logger(), "Current Average Decision FPS: %ld fps", 1000 / (time_sum / counter));
         }
 
         void lidarCallback(const custom_msg_pkg::msg::LidarPosition::SharedPtr msg) {
@@ -54,7 +102,7 @@ class DecisionController : public rclcpp::Node {
             RCLCPP_INFO(this->get_logger(), "Received %zu analyzed LiDAR samples", msg->x.size());
 
             // Image to LiDAR map function call goes here
-
+            //ADD LIDAR MSG CLASS HERE
             // End time
             auto end = std::chrono::high_resolution_clock::now();
             // Compute duration
@@ -66,11 +114,119 @@ class DecisionController : public rclcpp::Node {
             RCLCPP_INFO(this->get_logger(), "Current Average Decision FPS: %ld fps", 1000 / (time_sum / counter));
         }
 
-    void printImageObstacles(const camera_scan_pkg::msg::ObstacleArray::SharedPtr msg) {
-        for (camera_scan_pkg::msg::Obstacle obstacle : msg->obstacles) {
-            RCLCPP_INFO(this->get_logger(), "Obstacle detect at: (%lf, %lf)", obstacle.x, obstacle.y);
+
+        // The variable current_state is used to keep track of the current state of the drone.
+        // it only gets updated when an acknowledgement is received from the DroneCommander telling the decision_node
+        // that the drone has successfully completed the command. ie the last completed command is the current state.
+        void command_ack_callback(const custom_msg_pkg::msg::CommandAck::SharedPtr msg) {
+            if(msg->result == 0) {
+                switch(msg->command) {
+                    case custom_msg_pkg::msg::Command::STOP:
+                        RCLCPP_INFO(this->get_logger(), "Vehicle Stopped");
+                        current_state.store(STATES::STOP, std::memory_order_release); //STOP==1
+                        break;
+                    case custom_msg_pkg::msg::Command::TURN:
+                        RCLCPP_INFO(this->get_logger(), "Vehicle Turned");
+                        current_state.store(STATES::TURN, std::memory_order_release); //STOP==2
+                        break;
+                    case custom_msg_pkg::msg::Command::FORWARD:
+                        RCLCPP_INFO(this->get_logger(), "Vehicle Moved Forward");
+                        current_state.store(STATES::FORWARD, std::memory_order_release); //STOP==3
+                        break;
+                    // case custom_msg_pkg::msg::Command::DEFAULT:
+                    //     RCLCPP_INFO(this->get_logger(), "Vehicle DEFAULT");
+                    //     current_state.store(STATES::DEFAULT, std::memory_order_release); //DEFAULT==0
+                    //     break;
+                    default:
+                        RCLCPP_INFO(this->get_logger(), "Vehicle command acknowledged");
+                        break;
+                }
+                outstanding_ack.store(false, std::memory_order_release);
+            }
+            else {
+                RCLCPP_INFO(this->get_logger(), "Vehicle command failed");
+            }
         }
-    }
+
+        void find_path() {
+            while(rclcpp::ok()) {
+
+                
+                //the current state is stopped and we need to start turning
+                if(current_state.load(std::memory_order_acquire) == STATES::STOP && outstanding_ack.load(std::memory_order_acquire) == false){
+                    RCLCPP_INFO(this->get_logger(), "STOP STATE");
+                    turn_count++;
+                    if(turn_count < 24) {
+                        std::this_thread::sleep_for(std::chrono::milliseconds(10000)); //replace maybe with new logic for decision component
+                        RCLCPP_INFO(this->get_logger(), "PUBLISHED FIRST TURN");
+                        publish_control_command(custom_msg_pkg::msg::Command::TURN);
+                        outstanding_ack.store(true, std::memory_order_release);
+                    }
+                    
+                }
+                else if(current_state.load(std::memory_order_acquire) == STATES::TURN && outstanding_ack.load(std::memory_order_acquire) == false) {
+                    RCLCPP_INFO(this->get_logger(), "TURN STATE");
+                   current_state.store(STATES::REROUTING, std::memory_order_release);
+                }
+                else if (current_state.load(std::memory_order_acquire) == STATES::REROUTING && outstanding_ack.load(std::memory_order_acquire) == false) {
+                    //chill for a bit
+                    RCLCPP_INFO(this->get_logger(), "Rerouting");
+                    if(image_obstacles.tracked_obstacle) {
+                        turn_count++;
+                        RCLCPP_INFO(this->get_logger(), "TURN COUNT, %d ", turn_count);
+
+
+                        if(turn_count == 11){
+                            RCLCPP_INFO(this->get_logger(), "BIG TURN");
+                            std::this_thread::sleep_for(std::chrono::milliseconds(5000)); //replace maybe with new logic for decision component
+                            publish_control_command(custom_msg_pkg::msg::Command::TURN, -165);
+                        }
+                        else if (turn_count < 11) {
+                            RCLCPP_INFO(this->get_logger(), "LITTLE TURN");
+                            std::this_thread::sleep_for(std::chrono::milliseconds(5000)); //replace maybe with new logic for decision component
+                            publish_control_command(custom_msg_pkg::msg::Command::TURN);
+                        }
+                        else if (turn_count < 23){
+                            RCLCPP_INFO(this->get_logger(), "OPPOSITE LITTLE TURN");
+                            std::this_thread::sleep_for(std::chrono::milliseconds(5000)); //replace maybe with new logic for decision component
+                            publish_control_command(custom_msg_pkg::msg::Command::TURN, -15.0);
+                        }
+                        else {
+                            current_state.store(STATES::STOP, std::memory_order_release); //STOP==1
+                        }
+                        outstanding_ack.store(true, std::memory_order_release);
+                    }
+
+
+                    else {
+                        RCLCPP_INFO(this->get_logger(), "Published first forward ");
+                        publish_control_command(custom_msg_pkg::msg::Command::FORWARD);
+                        outstanding_ack.store(true, std::memory_order_release);
+                        turn_count = 0;
+                    }
+
+                }
+                else if(current_state.load(std::memory_order_acquire) == STATES::FORWARD && outstanding_ack.load(std::memory_order_acquire) == false) {
+                    RCLCPP_INFO(this->get_logger(), "Stopping Turn / Moving Forward");
+                    // publish_control_command(custom_msg_pkg::msg::Command::DEFAULT);
+                    current_state.store(STATES::DEFAULT, std::memory_order_release); //DEFAULT==0
+                    // outstanding_ack.store(true, std::memory_order_release);
+                }
+            }
+        }
+
+        void publish_control_command(const int32_t &in_command, const int32_t &degree = 15) {
+            custom_msg_pkg::msg::Command command_msg;
+            command_msg.command = in_command;
+            command_msg.deg = degree;
+            command_publisher_->publish(command_msg);
+        }
+
+        void printImageObstacles(const camera_scan_pkg::msg::ObstacleArray::SharedPtr msg) {
+            for (camera_scan_pkg::msg::Obstacle obstacle : msg->obstacles) {
+                RCLCPP_INFO(this->get_logger(), "Obstacle detect at: (%lf, %lf)", obstacle.x, obstacle.y);
+            }
+        }
 };
 
 int main(int argc, char** argv) {
---
+++ processor/src/drone_commander_pkg/src/PosFunctions.cpp
@@ -0,0 +1,158 @@
+#include "rclcpp/rclcpp.hpp"
+#include "drone_commander_pkg/PosFunctions.hpp"
+#include "drone_commander_pkg/DroneCommander.hpp"
+
+
+PositionControl::PositionControl(std::shared_ptr<DroneCommander> commander)
+    : commander_(commander) 
+{
+    initSubscribers();
+    initFrame();
+}
+
+void PositionControl::initSubscribers()
+{
+    rmw_qos_profile_t qos_profile = rmw_qos_profile_sensor_data;
+    auto qos = rclcpp::QoS(rclcpp::QoSInitialization(qos_profile.history, 5), qos_profile);
+    position_subscriber_ = commander_->create_subscription<px4_msgs::msg::VehicleLocalPosition>("/fmu/out/vehicle_local_position", qos, std::bind(&PositionControl::positionCallback, this, std::placeholders::_1));
+    odometry_subscriber_ = commander_->create_subscription<px4_msgs::msg::VehicleOdometry>("/fmu/out/vehicle_odometry", qos, std::bind(&PositionControl::odometryCallback, this, std::placeholders::_1));
+    global_position_subscriber_ = commander_->create_subscription<px4_msgs::msg::VehicleGlobalPosition>("/fmu/out/vehicle_global_position", qos,std::bind(&PositionControl::globalPositionCallback, this, std::placeholders::_1));
+    
+}
+
+void PositionControl::positionCallback(const px4_msgs::msg::VehicleLocalPosition::SharedPtr msg)
+{
+    current_local_position_ = *msg;
+    //RCLCPP_INFO(commander_->get_logger(), "Local Position - X: %f, Y: %f, Z: %f", msg->x, msg->y, msg->z);
+}
+
+void PositionControl::odometryCallback(const px4_msgs::msg::VehicleOdometry::SharedPtr msg)
+{
+    current_odometry_ = *msg;
+
+    float q0 = msg->q[0];  
+    float q1 = msg->q[1];  
+    float q2 = msg->q[2];  
+    float q3 = msg->q[3];  
+
+    float psi = atan2(2.0 * (q0 * q3 + q1 * q2), 1.0 - 2.0 * (q2 * q2 + q3 * q3));
+    current_heading_ = psi;
+
+   // RCLCPP_INFO(commander_->get_logger(), "Odometry - X: %f, Y: %f, Z: %f", current_odometry_.position[0], current_odometry_.position[1], current_odometry_.position[2]);
+}
+
+void PositionControl::globalPositionCallback(const px4_msgs::msg::VehicleGlobalPosition::SharedPtr msg)
+{
+    current_global_position_ = *msg;
+    //RCLCPP_INFO(commander_->get_logger(), "Global Position - Lat: %f, Lon: %f, Alt: %f", msg->lat, msg->lon, msg->alt);
+}
+
+void PositionControl::initFrame()
+{
+    local_offset_pose_.x = 0.0;
+    local_offset_pose_.y = 0.0;
+    local_offset_pose_.z = 0.0;
+    local_offset_ = 0.0;
+
+    int iterations = 30;
+
+    for (int i = 1; i <= iterations; i++) {
+        if (current_odometry_.q[0] != 0.0 && current_odometry_.q[1] != 0.0 && 
+            current_odometry_.q[2] != 0.0 && current_odometry_.q[3] != 0.0) {
+            
+            float q0 = current_odometry_.q[0];  
+            float q1 = current_odometry_.q[1];  
+            float q2 = current_odometry_.q[2];  
+            float q3 = current_odometry_.q[3];  
+
+            float psi = atan2(2.0 * (q0 * q3 + q1 * q2), 1.0 - 2.0 * (q2 * q2 + q3 * q3));
+            local_offset_ += (psi * (180.0 / M_PI));
+        }
+
+        local_offset_pose_.x += current_odometry_.position[0];
+        local_offset_pose_.y += current_odometry_.position[1];
+        local_offset_pose_.z += current_odometry_.position[2];
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+
+    local_offset_pose_.x /= iterations;
+    local_offset_pose_.y /= iterations;
+    local_offset_pose_.z /= iterations;
+    local_offset_ /= iterations;
+    RCLCPP_INFO(commander_->get_logger(), "LOCAL OFFSET: %f", local_offset_);
+}
+
+px4_msgs::msg::TrajectorySetpoint PositionControl::turnByAngle(float angle_degrees, bool commanded)
+{
+    std::array<float, 3> current_pos = getLocalPosition();
+    float target_yaw;
+
+    if(commanded){
+        float angle_radians = angle_degrees * (M_PI / 180.0);
+        float current_yaw = getCurrentHeading();
+        RCLCPP_INFO(commander_->get_logger(), "CURRENT YAW: %f, ANGLE_DEGREES %f", current_yaw * (180.0 / M_PI), angle_degrees);
+
+        target_yaw = current_yaw + angle_radians;
+        place_holder_yaw = target_yaw;
+        current_pos[2] -= 0.35; //avoid decrease in height from turning
+    }
+    else{
+        target_yaw = place_holder_yaw;
+    }
+
+    if (target_yaw > M_PI) {
+        target_yaw -= 2 * M_PI;
+    } else if (target_yaw < -M_PI) {
+        target_yaw += 2 * M_PI;
+    }
+
+    px4_msgs::msg::TrajectorySetpoint trajectory_setpoint_msg;
+    trajectory_setpoint_msg.position[0] = current_pos[0];
+    trajectory_setpoint_msg.position[1] = current_pos[1];
+    trajectory_setpoint_msg.position[2] = current_pos[2];
+    trajectory_setpoint_msg.yaw = target_yaw;
+    trajectory_setpoint_msg.yawspeed = 0.0;
+    RCLCPP_INFO(commander_->get_logger(), "TURN TURN TURN YAW: %f, ELIVATION %f", target_yaw * (180.0 / M_PI), trajectory_setpoint_msg.position[2]);
+
+
+    return trajectory_setpoint_msg;
+}
+
+px4_msgs::msg::TrajectorySetpoint PositionControl::moveForwardByMeters(float dist){
+
+    std::array<float, 3> current_pos = getLocalPosition();
+
+    auto trajectory_setpoint_msg = TrajectorySetpoint();
+    trajectory_setpoint_msg.position[0] = dist;  // Move forward 5 meters
+    trajectory_setpoint_msg.position[1] = current_pos[1];
+    trajectory_setpoint_msg.position[2] = current_pos[2];
+    trajectory_setpoint_msg.yaw = getCurrentHeading();
+    RCLCPP_INFO(commander_->get_logger(), "POS 0: %f::: CURRENT POS: %f", trajectory_setpoint_msg.position[0], current_pos[0]);
+
+    return trajectory_setpoint_msg;
+}
+
+bool PositionControl::checkDist(float start_dist){
+    std::array<float, 3> current_pos = getLocalPosition();
+
+    if(fabs(start_dist - current_pos[0]) > 4.5){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+float PositionControl::getCurrentHeading()
+{
+    return current_heading_;
+}
+
+std::array<float, 3> PositionControl::getLocalPosition()
+{
+    std::array<float, 3> pos = {};
+    pos[0] = current_local_position_.x;
+    pos[1] = current_local_position_.y;
+    pos[2] = current_local_position_.z;
+    return pos;
+}
