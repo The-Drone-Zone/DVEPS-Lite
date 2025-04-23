a7ff4a53faf858db9930aae3a5d8b77c427ef1ed
dshay <dorianshay0806@arizona.edu>
2025-04-23 15:40:37 -0700

    new lidar tracking

--- processor/src/lidar_scan_pkg/src/lidar_analysis_node.cpp
+++ processor/src/lidar_scan_pkg/src/lidar_analysis_node.cpp
@@ -26,7 +26,6 @@ class SLLidarClient : public rclcpp::Node {
             "scan", rclcpp::SensorDataQoS(), std::bind(&SLLidarClient::scanCallback, this, std::placeholders::_1));
 
         analysis_pub = this->create_publisher<custom_msg_pkg::msg::LidarPosition>("Lidar/analysis", rclcpp::QoS(rclcpp::KeepLast(10)));
-        pixhawk_pub = this->create_publisher<px4_msgs::msg::ObstacleDistance>("/fmu/in/obstacle_distance", 10);
     }
 
    private:
@@ -35,76 +34,75 @@ class SLLidarClient : public rclcpp::Node {
         RCLCPP_INFO(this->get_logger(), "I heard a laser scan %s[%d]", scan->header.frame_id.c_str(), count);
         RCLCPP_INFO(this->get_logger(), "angle_range : [%f, %f]", RAD2DEG(scan->angle_min), RAD2DEG(scan->angle_max));
 
-        // For testing lidar messages (we can change which points we publish later)
-        px4_msgs::msg::ObstacleDistance GCS_msg = px4_msgs::msg::ObstacleDistance();
-        GCS_msg.timestamp = this->get_clock()->now().nanoseconds() / 1000;
-        GCS_msg.frame = px4_msgs::msg::ObstacleDistance::MAV_FRAME_BODY_FRD;
-        GCS_msg.sensor_type = px4_msgs::msg::ObstacleDistance::MAV_DISTANCE_SENSOR_LASER;
-        GCS_msg.increment = 1125;  // degrees per measurement
-        GCS_msg.min_distance = 100;  // 100 cm = 1 m
-        GCS_msg.max_distance = 4000;  // 4000 cm = 40 m
-        GCS_msg.angle_offset = 0.0f;
-
-        for (int i = 0; i < count; i+= 45) {
-            if (i <= count) {
-                float degree = RAD2DEG(scan->angle_min + scan->angle_increment * i);
-                RCLCPP_INFO(this->get_logger(), "angle-distance : [%f, %f]", degree, scan->ranges[i]);
-                GCS_msg.distances[i / 45] = scan->ranges[i] * 100; // need to convert distance to cm (if not already)
-                GCS_msg.increment = scan->angle_increment * 45 * 10000;
-            }
+        std::vector<float> current_ranges = scan->ranges;
+        if (scan_history_.size() >= HISTORY_SIZE) {
+            scan_history_.pop_front();
         }
-        // Try this if the top loop doesn't work
-        // for (int i = 0; i < 72; ++i) {
-        //     float degree = RAD2DEG(scan->angle_min + (scan->angle_increment * i * 45));
-        //     RCLCPP_INFO(this->get_logger(), "angle-distance : [%f, %f]", degree, scan->ranges[i * 45]);
-        //     GCS_msg.distances[i] = scan->ranges[i * 45];
-        //     GCS_msg.increment = scan->angle_increment * 45;
+        scan_history_.push_back(current_ranges);
+        // for (int i = 0; i < count; i+= 45) {
+        //     if (i <= count) {
+        //         float degree = RAD2DEG(scan->angle_min + scan->angle_increment * i);
+        //         RCLCPP_INFO(this->get_logger(), "angle-distance : [%f, %f]", degree, scan->ranges[i]);
+        //     }
         // }
 
         RCLCPP_INFO(this->get_logger(), "range_size : %d", count);
-        pixhawk_pub->publish(GCS_msg);
-        publish_analysis(this->analysis_pub, scan, count);
+        publish_analysis(this->analysis_pub, scan, count, scan->scan_time);
     }
 
     void publish_analysis(rclcpp::Publisher<custom_msg_pkg::msg::LidarPosition>::SharedPtr& pub, 
                           const sensor_msgs::msg::LaserScan::SharedPtr scan,
-                          int count )
+                          int count, float time_bewteen_full_scans = 0.1)
     {
-
-        sensor_msgs::msg::LaserScan current_scan = *scan;
-        float sum_of_potential_x = 0.0;
-        float sum_of_potential_y = 0.0;
-        bool avoid = false;
-
         custom_msg_pkg::msg::LidarPosition msg;
         msg.stop = false;
-        
-        for(int i = 0; i < count; ++i) {
-            float d0 = 25; // ignore values under 20 meters we missed an object and our design was flawed. also a requiremtn to detect objects at 20 meters
-            float k = 0.5; //gain / weightedness of potential field
-            
-            //filter our object whose values are under 20 meters
-            if(current_scan.ranges[i] < d0 && current_scan.ranges[i] > current_scan.range_min) {
-                msg.stop = true;
+        msg.least_val = 50;
 
-                //MEASURED IN RADIANS
-                float X_radians = cos(scan->angle_min + scan->angle_increment * i);
-                float Y_radians = sin(scan->angle_min + scan->angle_increment * i);
-                float potential = -.5 * k * pow( ( (1/current_scan.ranges[i]) - (1/d0) ), 2 );
-                sum_of_potential_x += X_radians * potential;
-                sum_of_potential_y += Y_radians * potential;
+        if (scan_history_.size() < 2) return;
+
+        //go through each beam in the 360 degree scan
+        for(int i = 0; i < count; ++i){
+            float degree = RAD2DEG(scan->angle_min + scan->angle_increment * i);
+
+            //Angles outisde the drone hitbox at 20 meters can be ignored.
+            if( !(degree > 0 && degree < 1.0) && !( dgree > 121.0 && degree < 124.0 ) && !( degree > 230.0  && degree < 233.0 ) && !(degree > 358.9 && degree < 359.99) ){
+                continue;                
             }
-            
+
+            if(scan->ranges[i] < msg.least_val) {
+                msg.least_val = scan->ranges[i]; //find the least range value in the scan
+            }
+
+            //least range value pass into message.
+            std::vector<float> range_over_time;
+            for (const auto& past_scan : scan_history_) {
+                if (i < past_scan.size()) {
+                    range_over_time.push_back(past_scan[i]);
+                }
+            }
+
+            if (range_over_time.size() < 2) continue; // Not enough data to analyze
+
+            float total_velocity = 0.0;
+            for (size_t j = 1; j < range_over_time.size(); ++j) {
+                float v = (range_over_time[j - 1] - range_over_time[j]) / time_bewteen_full_scans; //distance over time is velocity
+                total_velocity += v;
+            }
+            float average_velocity = total_velocity / (range_over_time.size() - 1); //minus 1 because we are looking at the intervals bewtween the ranges
+
+            float current_range = range_over_time.back(); //most recent range data
+
+            if (avg_velocity > 0.0) {
+                float collision_time = current_range / avg_velocity; //collision_time = time to collision based on the most recent rnge value and the average velocity of the object
+                if (collision_time < 4.0) { //4.0 because we are going at 5/ms and 4*5 = 20 meters
+                    danger_beam_count++; //Here I am using multiple beams to signal a stop not just 1 beam
+                }
+            }
         }
 
-        msg.sum_of_potential_x = sum_of_potential_x;
-        msg.sum_of_potential_y = sum_of_potential_y;
-
-        //XYZ COORDINATE MAPPING GOES HERE
-        
-        msg.z.assign(scan->ranges.begin(), scan->ranges.end());
-        msg.x.resize(count, 0.0);
-        msg.y.resize(count, 0.0);
+        if (danger_beam_count >= 3) { //I just chose a randum number 3 as the beam number. 
+            msg.stop = true;
+        }
 
         pub->publish(msg);
     }
@@ -112,6 +110,9 @@ class SLLidarClient : public rclcpp::Node {
     rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr lidar_info_sub_;
     rclcpp::Publisher<custom_msg_pkg::msg::LidarPosition>::SharedPtr analysis_pub;
     rclcpp::Publisher<px4_msgs::msg::ObstacleDistance>::SharedPtr pixhawk_pub;
+
+    std::deque<std::vector<float>> scan_history_;
+    const size_t HISTORY_SIZE = 5;
 };
 
 int main(int argc, char **argv) {
@@ -121,3 +122,43 @@ int main(int argc, char **argv) {
     rclcpp::shutdown();
     return 0;
 }
+
+
+
+
+// sensor_msgs::msg::LaserScan current_scan = *scan;
+//         float sum_of_potential_x = 0.0;
+//         float sum_of_potential_y = 0.0;
+//         bool avoid = false;
+
+//         custom_msg_pkg::msg::LidarPosition msg;
+//         msg.stop = false;
+        
+//         for(int i = 0; i < count; ++i) {
+//             float d0 = 25; // ignore values under 20 meters we missed an object and our design was flawed. also a requiremtn to detect objects at 20 meters
+//             float k = 0.5; //gain / weightedness of potential field
+            
+//             //filter our object whose values are under 20 meters
+//             if(current_scan.ranges[i] < d0 && current_scan.ranges[i] > current_scan.range_min) {
+//                 msg.stop = true;
+
+//                 //MEASURED IN RADIANS
+//                 float X_radians = cos(scan->angle_min + scan->angle_increment * i);
+//                 float Y_radians = sin(scan->angle_min + scan->angle_increment * i);
+//                 float potential = -.5 * k * pow( ( (1/current_scan.ranges[i]) - (1/d0) ), 2 );
+//                 sum_of_potential_x += X_radians * potential;
+//                 sum_of_potential_y += Y_radians * potential;
+//             }
+            
+//         }
+
+//         msg.sum_of_potential_x = sum_of_potential_x;
+//         msg.sum_of_potential_y = sum_of_potential_y;
+
+//         //XYZ COORDINATE MAPPING GOES HERE
+        
+//         msg.z.assign(scan->ranges.begin(), scan->ranges.end());
+//         msg.x.resize(count, 0.0);
+//         msg.y.resize(count, 0.0);
+
+//         pub->publish(msg);
