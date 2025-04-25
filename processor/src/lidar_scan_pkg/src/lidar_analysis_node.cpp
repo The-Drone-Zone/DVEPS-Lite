cade125f0dbe19c35de618f8d6b84a8a2cf95e0b
dveps-lite software team <dorianshay0806@gamil.com>
2025-04-24 22:17:53 -0700

    lidar analysis TTC

--- processor/src/lidar_scan_pkg/src/lidar_analysis_node.cpp
+++ processor/src/lidar_scan_pkg/src/lidar_analysis_node.cpp
@@ -15,7 +15,7 @@
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/laser_scan.hpp"
 #include "custom_msg_pkg/msg/lidar_position.hpp"
-#include <px4_msgs/msg/obstacle_distance.hpp>
+#include <deque>
 
 #define RAD2DEG(x) ((x) * 180.0 / M_PI)
 
@@ -29,24 +29,26 @@ class SLLidarClient : public rclcpp::Node {
     }
 
    private:
+
+    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr lidar_info_sub_;
+    rclcpp::Publisher<custom_msg_pkg::msg::LidarPosition>::SharedPtr analysis_pub;
+
+    std::deque<std::vector<float>> scan_history_;
+    const size_t HISTORY_SIZE = 5;
+
+
     void scanCallback(const sensor_msgs::msg::LaserScan::SharedPtr scan) {
         int count = scan->scan_time / scan->time_increment;
-        RCLCPP_INFO(this->get_logger(), "I heard a laser scan %s[%d]", scan->header.frame_id.c_str(), count);
-        RCLCPP_INFO(this->get_logger(), "angle_range : [%f, %f]", RAD2DEG(scan->angle_min), RAD2DEG(scan->angle_max));
+        // RCLCPP_INFO(this->get_logger(), "I heard a laser scan %s[%d]", scan->header.frame_id.c_str(), count);
+        // RCLCPP_INFO(this->get_logger(), "angle_range : [%f, %f]", RAD2DEG(scan->angle_min), RAD2DEG(scan->angle_max));
 
         std::vector<float> current_ranges = scan->ranges;
         if (scan_history_.size() >= HISTORY_SIZE) {
             scan_history_.pop_front();
         }
         scan_history_.push_back(current_ranges);
-        // for (int i = 0; i < count; i+= 45) {
-        //     if (i <= count) {
-        //         float degree = RAD2DEG(scan->angle_min + scan->angle_increment * i);
-        //         RCLCPP_INFO(this->get_logger(), "angle-distance : [%f, %f]", degree, scan->ranges[i]);
-        //     }
-        // }
 
-        RCLCPP_INFO(this->get_logger(), "range_size : %d", count);
+        // RCLCPP_INFO(this->get_logger(), "range_size : %d", count);
         publish_analysis(this->analysis_pub, scan, count, scan->scan_time);
     }
 
@@ -54,65 +56,90 @@ class SLLidarClient : public rclcpp::Node {
                           const sensor_msgs::msg::LaserScan::SharedPtr scan,
                           int count, float time_bewteen_full_scans = 0.1)
     {
+        int danger_beam_count = 0;
         custom_msg_pkg::msg::LidarPosition msg;
         msg.stop = false;
-        msg.least_val = 50;
+        //msg.least_val = 50;
 
         if (scan_history_.size() < 2) return;
 
         //go through each beam in the 360 degree scan
         for(int i = 0; i < count; ++i){
             float degree = RAD2DEG(scan->angle_min + scan->angle_increment * i);
+            if (degree < 0) {
+                degree += 360.0;
+            }
+            // RCLCPP_WARN(this->get_logger(), "DEGREE: %f, Range: %f", degree, scan->ranges[i]); // DEBUG DELETE LATER
 
             //Angles outisde the drone hitbox at 20 meters can be ignored.
-            if( !(degree > 0 && degree < 1.0) && !( dgree > 121.0 && degree < 124.0 ) && !( degree > 230.0  && degree < 233.0 ) && !(degree > 358.9 && degree < 359.99) ){
-                continue;                
+            if( !(degree > 178.5 && degree < 181.5) && !( degree > 60.5 && degree < 64.5 ) && !( degree > 306.5  && degree < 310.5 ) ){
+                continue;
             }
 
-            if(scan->ranges[i] < msg.least_val) {
-                msg.least_val = scan->ranges[i]; //find the least range value in the scan
-            }
+            // if(scan->ranges[i] < msg.least_val) {
+            //     msg.least_val = scan->ranges[i]; //find the least range value in the scan
+            // }
 
             //least range value pass into message.
+            float ZERO = .001;
             std::vector<float> range_over_time;
             for (const auto& past_scan : scan_history_) {
-                if (i < past_scan.size()) {
-                    range_over_time.push_back(past_scan[i]);
+                float value = past_scan[i];
+                if (value > ZERO && value < 40.00) {
+                    range_over_time.push_back(value);
                 }
             }
 
-            if (range_over_time.size() < 2) continue; // Not enough data to analyze
+            if (range_over_time.size() < 3) continue; // Not enough data to analyze
+
+            //DEBUG ONLY DELETE BEFORE PR
+            // std::cout << "Values at index " << i << " (degree: " << degree << "): ";
+            // for (const auto& r : range_over_time) {
+            //     std::cout << r << " ";
+            // }
+            // std::cout << std::endl;
 
             float total_velocity = 0.0;
             for (size_t j = 1; j < range_over_time.size(); ++j) {
                 float v = (range_over_time[j - 1] - range_over_time[j]) / time_bewteen_full_scans; //distance over time is velocity
                 total_velocity += v;
             }
-            float average_velocity = total_velocity / (range_over_time.size() - 1); //minus 1 because we are looking at the intervals bewtween the ranges
+            float avg_velocity = total_velocity / (range_over_time.size() - 1); //minus 1 because we are looking at the intervals bewtween the ranges
+
+            // if (avg_velocity > 2 || total_velocity > 2){
+            //     RCLCPP_WARN(this->get_logger(), "total_velocity: %f", total_velocity); // DEBUG DELETE LATER
+            //     RCLCPP_WARN(this->get_logger(), "avg_velocity  : %f", avg_velocity); // DEBUG DELETE LATER
+            //     std::cout << "Values at index " << i << " (degree: " << degree << "): ";
+            //     for (const auto& r : range_over_time) {
+            //         std::cout << r << " ";
+            //     }
+            //     std::cout << std::endl;
+            // }
 
             float current_range = range_over_time.back(); //most recent range data
 
             if (avg_velocity > 0.0) {
+                //RCLCPP_WARN(this->get_logger(), "avg_velocity  : %f", avg_velocity); // DEBUG DELETE LATER
                 float collision_time = current_range / avg_velocity; //collision_time = time to collision based on the most recent rnge value and the average velocity of the object
+                // RCLCPP_WARN(this->get_logger(), "collision_time  : %f", collision_time); // DEBUG DELETE LATER
+
                 if (collision_time < 4.0) { //4.0 because we are going at 5/ms and 4*5 = 20 meters
                     danger_beam_count++; //Here I am using multiple beams to signal a stop not just 1 beam
+                }
+
+                if (danger_beam_count >= 3) { //I just chose a randum number 3 as the beam number.
+                   break;
                 }
             }
         }
 
-        if (danger_beam_count >= 3) { //I just chose a randum number 3 as the beam number. 
+        if (danger_beam_count >= 3) { //I just chose a randum number 3 as the beam number.
+            RCLCPP_INFO(this->get_logger(), "DANGER BEAM: STOP STOP STOP");
             msg.stop = true;
         }
 
-        pub->publish(msg);
+        // pub->publish(msg);
     }
-
-    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr lidar_info_sub_;
-    rclcpp::Publisher<custom_msg_pkg::msg::LidarPosition>::SharedPtr analysis_pub;
-    rclcpp::Publisher<px4_msgs::msg::ObstacleDistance>::SharedPtr pixhawk_pub;
-
-    std::deque<std::vector<float>> scan_history_;
-    const size_t HISTORY_SIZE = 5;
 };
 
 int main(int argc, char **argv) {
